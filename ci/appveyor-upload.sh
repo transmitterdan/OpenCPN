9bcb2d6715c7f1b28a2fb9737aae81c84b66026d
transmitterdan <noone@noisp.org>
2019-12-31 01:56:35 -0600

    Create private CI branch

--- ci/appveyor-upload.sh
+++ ci/appveyor-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REPO=${CLOUDSMITH_REPO:-"david-register/opencpn-unstable"}
+REPO=${CLOUDSMITH_REPO:-"dan-dickey/opencpn-unstable-testing"}
 
 if [ -z "$CLOUDSMITH_API_KEY" ]; then
     echo 'Cannot deploy to cloudsmith: missing $CLOUDSMITH_API_KEY'
