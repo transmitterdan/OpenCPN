1c5b64e6d9b1bf7040f988486abd1d647653078a
transmitterdan <10525699+transmitterdan@users.noreply.github.com>
2021-07-03 06:48:49 -0500

    Properly include wx.h when pre-compiled headers is not enabled

--- libs/wxJSON/src/jsonval.cpp
+++ libs/wxJSON/src/jsonval.cpp
@@ -19,6 +19,10 @@
 // For compilers that support precompilation, includes "wx.h".
 #include "wx/wxprec.h"
 
+#ifndef  WX_PRECOMP
+#include "wx/wx.h"
+#endif //precompiled headers
+
 #ifdef __BORLANDC__
 #pragma hdrstop
 #endif
